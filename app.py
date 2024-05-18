d437070074081c22fad12f06173616a7a212f747
Kevin Dela Cruz <162408197+kvndlcrz@users.noreply.github.com>
2024-05-18 18:38:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,16 +25,11 @@ def preprocess_image(image):
 
 #UI
 st.write("""### <span style='color:yellow'>Weather Vision: <span style='color:white'>Predicting Weather Conditions from Image</span>
-    \nPredict the weather condition from uploaded images\n
-    Possible conditions: cloudy, rainy, sunny, sunset
+    \nPredict the weather condition from uploaded images. Possible conditions: cloudy, rainy, sunny, sunset
 """, unsafe_allow_html=True)
 
 # Upload image
 uploaded_image = st.file_uploader("Choose an image (jpg, png, jpeg) to classify: ", type=["jpg", "png", "jpeg"])
-
-st.write("""<div style="text-align: center;"><h2>Github Link\n</h2><a
-\nhttps://github.com/kvndlcrz/Final-Project_CPE019_Emtech2.git\n""", 
-         unsafe_allow_html=True)
 
 st.write("""<div style="text-align: center;"><h2>Github Link
     \nhttps://github.com/kvndlcrz/Final-Project_CPE019_Emtech2.git""", 
