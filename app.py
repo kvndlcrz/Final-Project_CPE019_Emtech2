5b007901a323e874d233518657f7242b2ae72d6c
Kevin Dela Cruz <162408197+kvndlcrz@users.noreply.github.com>
2024-05-18 21:09:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.write("""
 """, unsafe_allow_html=True)
 
 # GitHub link
-st.write(""" <div style="text-align: center;">
+st.write(""" <div style="text-align: center;"><br>
     https://github.com/kvndlcrz/Final-Project_CPE019_Emtech2.git
  <br><br>""", unsafe_allow_html=True)
 
