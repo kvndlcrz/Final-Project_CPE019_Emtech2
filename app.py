3f3bc05e792b08cb65092cf1e960a6a6a73822a7
Kevin Dela Cruz <162408197+kvndlcrz@users.noreply.github.com>
2024-05-18 14:29:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('model.h5')
+    model = tf.keras.models.load_model('final_model.h5')
     return model
 
 model = load_model()
