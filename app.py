68117cfa0388486f1ddba572a63f318e5b8f94c4
Kevin Dela Cruz <162408197+kvndlcrz@users.noreply.github.com>
2024-05-18 19:33:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 from PIL import Image
 
 # Load the pre-trained weather classification model
-@st.cache(allow_output_mutation=True)
 def load_model():
     try:
         st.write("Loading the pre-trained weather classification model...")
@@ -23,17 +22,24 @@ def preprocess_image(image):
     preprocessed_image = np.expand_dims(normalized_image, axis=0)  # Add batch dimension
     return preprocessed_image
 
-# Streamlit UI
+# UI setup
 st.write("""
-    # Weather Classifier App
-    \nPredict the weather condition from uploaded images\n
-    Possible conditions: cloudy, rainy, sunny, sunset
-""")
+### <span style='color:yellow'>Weather Vision:</span> <span style='color:white'>Predicting Weather Conditions from Image</span>
+<div style="text-align: center;">Predict the weather condition from uploaded images. Possible conditions: cloudy, rainy, sunny, sunset</div>
+<br><br>
+""", unsafe_allow_html=True)
 
-st.text("Upload an image.")
+# GitHub link
+st.write("""
+### <br><div style="text-align: center;"> GitHub Link
+     [https://github.com/kvndlcrz/Final-Project_CPE019_Emtech2.git](https://github.com/kvndlcrz/Final-Project_CPE019_Emtech2.git)
+""", unsafe_allow_html=True)
 
 # Upload image
-uploaded_image = st.file_uploader("Choose an image (jpg, png, jpeg) to classify: ", type=["jpg", "png", "jpeg"])
+uploaded_image = st.file_uploader(
+    label="Choose an image (jpg, png, jpeg) to classify:",
+    type=["jpg", "png", "jpeg"]
+)
 
 if uploaded_image is not None:
     # Display the uploaded image
@@ -51,10 +57,14 @@ if uploaded_image is not None:
         prediction = model.predict(preprocessed_image)
 
         # Define weather categories
-        weather_conditions = ['Cloudy', 'Rainy', 'Sunny', 'Sunset']
+        weather_conditions = ['Cloudy', 'Rainy', 'Shine', 'Sunset']
 
         # Determine the predicted weather condition
         predicted_condition = weather_conditions[np.argmax(prediction)]
 
         # Display the prediction
         st.write("Predicted Weather Condition:", predicted_condition)
+    else:
+        st.write("Failed to load the model.")
+else:
+    st.write("Please upload an image to get a prediction.")
