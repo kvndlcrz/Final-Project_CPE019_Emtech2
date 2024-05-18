49a59f10e59fb64b352d1d6d616f9f6b49cd679f
Kevin Dela Cruz <162408197+kvndlcrz@users.noreply.github.com>
2024-05-18 16:32:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import tensorflow as tf
-from PIL import Image, ImageOps
+import cv2
 import numpy as np
+from PIL import Image, ImageOps
 
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -18,7 +19,7 @@ file = st.file_uploader("Choose a weather photo from your computer", type=["jpg"
 
 def import_and_predict(image_data, model):
     size = (64, 64)
-    image = ImageOps.fit(image_data, size, Image.LANCZOS)  # Use Image.LANCZOS instead of Image.ANTIALIAS
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
     img = np.asarray(image)
     img_reshape = img[np.newaxis, ...]
     prediction = model.predict(img_reshape)
