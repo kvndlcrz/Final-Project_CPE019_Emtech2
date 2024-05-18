aed1b7006a0cdfca7f1400953bb9e952fdc92614
Kevin Dela Cruz <162408197+kvndlcrz@users.noreply.github.com>
2024-05-18 16:31:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('best_model.h5')
+    model = tf.keras.models.load_model('weather_classifier.hdf5')
     return model
 
 model = load_model()
@@ -14,13 +16,9 @@ st.write("""
 
 file = st.file_uploader("Choose a weather photo from your computer", type=["jpg", "png"])
 
-import cv2
-from PIL import Image, ImageOps
-import numpy as np
-
 def import_and_predict(image_data, model):
     size = (64, 64)
-    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)  # Use Image.LANCZOS instead of Image.ANTIALIAS
     img = np.asarray(image)
     img_reshape = img[np.newaxis, ...]
     prediction = model.predict(img_reshape)
