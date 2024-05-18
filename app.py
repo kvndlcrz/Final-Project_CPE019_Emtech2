bd919e5c3c04c206e62dc5ae6e830d317c8fbcd4
Kevin Dela Cruz <162408197+kvndlcrz@users.noreply.github.com>
2024-05-18 19:01:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.write("""### <span style='color:yellow'>Weather Vision: <span style='color:wh
 
 # Upload image
 uploaded_image = st.file_uploader(
-    """<div style="text-align:center;">Choose an image (jpg, png, jpeg) to classify:</div>""", 
+    """<div style="text-align:center;">Choose an image (jpg, png, jpeg) to classify:</div>""", unsafe_allow_html=True 
     type=["jpg", "png", "jpeg"]
 )
 
